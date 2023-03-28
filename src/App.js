bf729a5366ddd037e3db8c8f1a09aebc22092750
Key <213482@ids.upchiapas,edu.mx>
2023-03-27 23:38:07 -0600

    Upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+//import Sidenav from './Sidenav'
+import{Routes, Route, BrowserRouter} from "react-router-dom"
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Settings from './pages/Settings'
+
+export default function App() {
+  return (
+  <>
+   <BrowserRouter>
+     <Routes>
+       <Route path="/" exact element={<Home />}></Route>
+       <Route path="/products" exact element={<Products />}></Route>
+       <Route path="/settings" exact element={<Settings />}></Route>
+     </Routes>
+   </BrowserRouter>
+  </>
+  )
+}
+
